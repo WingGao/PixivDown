f0b9766e707f1fdc465a587412f65456e2020522
Wing Gao <wing.gao@live.com>
2015-12-05 20:54:04 +0800

    修改config_view

--- pixiv/views.py
+++ pixiv/views.py
@@ -5,13 +5,14 @@ import models
 
 # Create your views here.
 def config_view(request):
+    config = models.get_config()
     if request.method == 'GET':
         return render_to_response('config_view.html', {
-            'cookie': models.json_config.get(models.CONFIG_KEY_COOKIE, ''),
-            'cookie_expire': models.json_config.get(models.CONFIG_KEY_COOKIE_EXPIRE, ''),
+            'cookie': config.get(models.CONFIG_KEY_COOKIE, ''),
+            'cookie_expire': config.get(models.CONFIG_KEY_COOKIE_EXPIRE, ''),
         })
     elif request.method == 'POST':
-        models.json_config.set({
+        config.set({
             models.CONFIG_KEY_COOKIE: request.POST.get('cookie', ''),
             models.CONFIG_KEY_COOKIE_EXPIRE: request.POST.get('cookie_expire', '')
         })
